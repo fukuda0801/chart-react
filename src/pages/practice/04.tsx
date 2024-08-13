ed9df486a7e1deb65217b92f1baa6060ccee57f4
fukuda0801 <nagisano0801@gmail.com>
2024-08-13 13:20:57 +0900

    section5: リアルタイム文字入力完了

---
+++ src/pages/practice/04.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next';
+
+import { useRealTimeText } from '@/hooks/useRealTimeText';
+
+const Page: NextPage = () => {
+  const { text, handleChangeText } = useRealTimeText();
+  return (
+    <div className="mx-auto mt-10 max-w-4xl">
+      <div className="flex justify-center">
+        <div>
+          <h2 className="mb-4 text-center text-6xl">{text}</h2>
+          <input onChange={handleChangeText} className="border px-3 py-2" type="text" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
